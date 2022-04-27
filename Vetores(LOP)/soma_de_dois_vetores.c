39a152e79fcfe4f4f41407a6f6e60034cd4355d3
João Paulo <joaoroque317@gmail.com>
2022-04-27 14:27:04 -0300

    Update soma_de_dois_vetores.c
    Questão passou nos testes do lop

--- Vetores(LOP)/soma_de_dois_vetores.c
+++ Vetores(LOP)/soma_de_dois_vetores.c
@@ -6,21 +6,24 @@ int main(){
     int vetor_a[n];
     int vetor_b[n];
     int soma[n];
+    int cont_a =0;
+    int cont_b =0;
     
-    printf("TAMANHO DE N %d", n);
     if (n<=10){
-        for(int j = 0; j < n; j++ ){
-            printf("A\n");
-            scanf("%d", &vetor_a[j]);
-            scanf("%d", &vetor_a[j]);
+        while(cont_a<n){
+            scanf("%d", &vetor_a[cont_a]);
+            cont_a++;
             }
-        
-        for (int i =0;i<=n;i++){
-        soma[i] = vetor_a[i] + vetor_b[i];
+        while(cont_b<n){
+            scanf("%d", &vetor_b[cont_b]);
+            cont_b++;
+            }
+        for (int i =0;i<n;i++){
+        soma[i]= vetor_a[i] + vetor_b[i];
+        printf("%d ",soma[i]);
         }
-        
-        
-         printf("%d", soma[3]);
-
+    }
+    else{
+        printf("Muitos elementos");
     }
 }
